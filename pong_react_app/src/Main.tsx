213a715965feaa8c892688ecd9cce23dd6c538ea
clodaghsmurphy <clodagh9@yahoo.ie>
2023-02-21 12:48:16 +0100

    merge nathan

--- pong_react_app/src/Main.tsx
+++ pong_react_app/src/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './NavBar'
 import './Dashboard.css'
+import  user_pfp from './media/user.png'
 
 function Main()
 {
@@ -10,9 +11,42 @@ function Main()
        <main className="page-wrapper">
             <div className="game">
                 <div className="game-screen"></div>
-                <div className="player-vs"></div>
+                <div className="player-vs">
+                    <div className="player">
+                        <div className="avatar">
+                            <img src={user_pfp} />
+                        </div>
+                        <div className="player-info">
+                            <span className="player-name">clmurphy</span>
+                            <span className="player-level">LVL 12</span>
+                        </div>
+                    </div>
+                    <div className="VS">VS</div>
+                    <div className="player">
+                        <div className="avatar">
+                            <img src={user_pfp} />
+                        </div>
+                        <div className="player-info">
+                            <span className="player-name">nguiard</span>
+                            <span className="player-level">LVL 12</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="live-chat"></div>
+            <div className="live-chat">
+                <div className="chat-title">
+                    <h1>Live Chat Room </h1>
+                </div>
+                <div className="chat-box">
+                    <div className="message">
+                        <div className="message-header">
+                            <span>clmurphy</span>
+                            <span>1m</span>
+                        </div>
+                    </div>
+                    <div className="message"></div>
+                </div>
+            </div>
         </main>
         </div>
     );
