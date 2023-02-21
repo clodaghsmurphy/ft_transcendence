9b3b7beeb0c602750b31b17b53e5ac4ea16721ed
Nathan <nguiard@student.42.fr>
2023-02-21 11:55:10 +0100

    Colones OK, J'essaye de mettre le "Message" au bon endroit

--- pong_react_app/src/Chat.tsx
+++ pong_react_app/src/Chat.tsx
@@ -9,7 +9,13 @@ function Chat()
         <div className="dashboard">
         <NavBar /> 
         <main className="page-wrapper">
-            <div className="messages"></div>
+            <div className="channels">
+				<h1>
+					<div className="delimiters"></div>
+					<div className='channels_title'>Message</div>
+					<div className="delimiters"></div>
+				</h1>
+			</div>
             <div className="chatbox"></div>
             <div className="group-members"></div>
         </main>
