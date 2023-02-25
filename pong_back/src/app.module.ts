3a531286f2cac2abf7a267b79499177a26cfe11c
Pierre LE LEZ <ple-lez@e2r11p1.clusters.42paris.fr>
2023-02-25 17:00:44 +0100

    start channel module boilerplate

--- pong_back/src/app.module.ts
+++ pong_back/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { ChannelModule } from './channel/channel.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), UserModule, PrismaModule, ChannelModule],
 })
 export class AppModule {}
