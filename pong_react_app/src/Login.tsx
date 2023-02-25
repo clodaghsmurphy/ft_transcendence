5485d0c3da0fd7796c888f81f1c9906f9a4e8954
clodaghsmurphy <clodagh9@yahoo.ie>
2023-02-25 15:26:55 +0100

    reading up on api

--- pong_react_app/src/Login.tsx
+++ pong_react_app/src/Login.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { useCallback, useState } from 'react';
+import api_keys from './api_cred'
 
 function Login ()
 {
+	interface OAuth2data
+	{
+		authorizeUrl:string;
+		cleintID:string;
+		redirectUri:string;
+		scope:string;
+	}
+
+
+	const getAuth = () =>
+	{
+		console.log(api_keys.api_keys.secret);
+	}
 	return (
 		<div className="login-box">
-			<Link className="login-button" to ="/dashboard">
-				<button id="42-login">
+			<Link className="login-button" to="/dashboard">
+				<button id="42-login" onClick={getAuth}>
 					Login via 42
-				</button>
+				</button >
 			</Link>
 			<button  className="login-button" id="guest-login">
 				Login as guest
