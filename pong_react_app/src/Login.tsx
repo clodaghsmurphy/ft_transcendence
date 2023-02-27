07fe1623f881530ed0ad0bb220e4f96cd182efde
clodaghsmurphy <clodagh9@yahoo.ie>
2023-02-27 10:55:12 +0100

    working on api

--- pong_react_app/src/Login.tsx
+++ pong_react_app/src/Login.tsx
@@ -1,27 +1,43 @@
 import React from 'react';
+import axios from "axios";
+import { randomBytes } from "crypto"
+import { nanoid } from 'nanoid'
 import { Link } from 'react-router-dom'
 import { useCallback, useState } from 'react';
 import api_keys from './api_cred'
 
+
 function Login ()
 {
-	interface OAuth2data
+	interface loginData
 	{
 		authorizeUrl:string;
-		cleintID:string;
+		clientID:string;
 		redirectUri:string;
 		scope:string;
+		state:string;
 	}
 
+		//redirect_uri=https%3A%2F%2F42-beta.vmontagn.fr%2Fauth%2F42%2Fcallback
 
-	const getAuth = () =>
+	const handleLogin = () =>
 	{
-		console.log(api_keys.api_keys.secret);
+		const login:loginData = { 
+			authorizeUrl: "https://api.intra.42.fr/oauth/authorize",
+			clientID: api_keys.client_id,
+			redirectUri: encodeURIComponent("http://localhost:8080/!!"),
+			scope: 'public',
+			state: nanoid(16)
+		}
+		console.log(api_keys.secret);
+		console.log(encodeURIComponent("http://localhost:8080"));
+		const url:string = `https://api.intra.42.fr/oauth/authorize?client_id=${login.clientID}&redirect_uri=${login.redirectUri}&scope=${login.scope}&state=${login.state}&response_type=code`
+		console.log(url);
 	}
 	return (
 		<div className="login-box">
 			<Link className="login-button" to="/dashboard">
-				<button id="42-login" onClick={getAuth}>
+				<button id="42-login" onClick={handleLogin}>
 					Login via 42
 				</button >
 			</Link>
