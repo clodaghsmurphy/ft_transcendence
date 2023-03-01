abe5b60cef138293ffea2b81a695d8d3078d81eb
Pierre LE LEZ <ple-lez@e2r10p1.clusters.42paris.fr>
2023-03-01 13:23:21 +0100

    Add Channel JOIN

--- pong_back/src/user/user.controller.ts
+++ pong_back/src/user/user.controller.ts
@@ -8,26 +8,26 @@ export class UserController {
 
 	@Get('info')
 	getAllUsers() {
-		return this.userService.getAllUsers();
+		return this.userService.getAll();
 	}
 
 	@Get('info/:name')
 	getUser(@Param() params) {
-		return this.userService.getUser(params.name);
+		return this.userService.get(params.name);
 	}
 
 	@Get('info/:name/:attribute')
 	getUserInfo(@Param() params) {
-		return this.userService.getUserInfo(params.name, params.attribute);
+		return this.userService.getInfo(params.name, params.attribute);
 	}
 
 	@Post('create')
 	createUser(@Body() dto: UserCreateDto) {
-		return this.userService.createUser(dto);
+		return this.userService.create(dto);
 	}
 
 	@Post('update')
 	updateUser(@Body() dto: UserUpdateDto) {
-		return this.userService.updateUser(dto);
+		return this.userService.update(dto);
 	}
 }
