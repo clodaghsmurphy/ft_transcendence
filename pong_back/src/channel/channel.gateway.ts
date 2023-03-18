d1f8300cc986414986ef2694dd3f9710c13dcdcc
Pierre LE LEZ <ple-lez@e3r11p1.clusters.42paris.fr>
2023-03-18 17:11:40 +0100

    Message method for channel gateway

--- pong_back/src/channel/channel.gateway.ts
+++ pong_back/src/channel/channel.gateway.ts
@@ -1,9 +1,9 @@
 import { Logger } from "@nestjs/common";
-import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Channel } from "@prisma/client";
 import { Socket, Namespace } from 'socket.io';
 import { ChannelService } from "./channel.service";
-import { ChannelCreateDto } from "./dto";
+import { ChannelCreateDto, ChannelJoinDto, MessageCreateDto } from "./dto";
 
 @WebSocketGateway({namespace: 'channel'})
 export class ChannelGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
@@ -27,13 +27,23 @@ export class ChannelGateway implements OnGatewayInit, OnGatewayConnection, OnGat
 		this.logger.log(`Number of connection: ${this.io.sockets.size}.`);
 	}
 
-	@SubscribeMessage('create')
-	async handleCreation(@MessageBody() dto: ChannelCreateDto): Promise<Channel> {
-		// this.logger.log(`Channel creation message recieved:`);
-		// this.logger.log(JSON.stringify(dto, null, 4));
+	// @SubscribeMessage('create')
+	// async handleCreation(@MessageBody() dto: ChannelCreateDto): Promise<Channel> {
+	// 	const channel = await this.channelService.create(dto);
+	// 	this.logger.log(`Created new channel: ${channel}`);
+	// 	return channel;
+	// }
 
-		const channel = await this.channelService.create(dto);
-		this.logger.log(`Created new channel: ${channel}`);
-		return channel;
+	@SubscribeMessage('join')
+	async handleJoin(@MessageBody() dto: ChannelJoinDto, @ConnectedSocket() client: Socket) {
+		await this.channelService.join(dto);
+		client.join(dto.name);
+		this.io.in(dto.name).emit('join', dto);
+	}
+
+	@SubscribeMessage('message')
+	async handleMessage(@MessageBody('channel') channel: string, @MessageBody('data') dto: MessageCreateDto) {
+		const message = await this.channelService.postMessage(channel, dto);
+		this.io.in(channel).emit('message', message);
 	}
 }
