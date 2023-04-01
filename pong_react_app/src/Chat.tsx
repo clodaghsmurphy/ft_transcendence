956cdb04b0552b27b0bdd4735e6a1bad93ec48ba
nguiard <nguiard@student.42.fr>
2023-04-01 15:07:13 +0200

    Button leave -> enleve la possibl=ilite de re clicker sur le chan

--- pong_react_app/src/Chat.tsx
+++ pong_react_app/src/Chat.tsx
@@ -322,7 +322,7 @@ function Chat()
 
             <div className="chatbox">
 				{Messages(current_chan as ChanAndMessage,
-					all_users, current_user, set_current_chan)}
+					all_users, current_user, set_current_chan, setChanOfUser)}
 			</div>
 
             <div className="group-members">
