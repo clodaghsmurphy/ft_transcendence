c7f4b4977327721bf59c4fca9808d0e5b2a6175e
Pierre LE LEZ <ple-lez@e3r10p1.clusters.42paris.fr>
2023-04-06 14:39:10 +0200

    Start game

--- pong_back/src/game/game.service.ts
+++ pong_back/src/game/game.service.ts
@@ -1,49 +1,69 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { Game } from '@prisma/client';
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { PrismaService } from "src/prisma/prisma.service";
 import { UserService } from "src/user/user.service";
-import * as path from 'path';
-import * as fs from 'fs';
-import { HttpService} from '@nestjs/axios'
 import { GameCreateDto } from "./dto";
-
+import { GameRoom } from "./types/game.types";
 
 @Injectable()
 export class GameService {
-  constructor(private prisma: PrismaService, private userService: UserService) {}
+	constructor(private prisma: PrismaService, private userService: UserService) {}
 
-  async getAll() {
-	return await this.prisma.game.findMany();
-  }
+	public activeGames: Map<number, GameRoom> = new Map<number, GameRoom>();
 
-  async get(id: number) {
-	return await this.prisma.game.findUnique({where: {id: id}});
-  }
-
-  async create(dto) {
-	await this.userService.checkUser(dto.user_id);
-	await this.userService.checkUser(dto.target_id);
-
-	const game = await this.prisma.game.create({
-		data: {
-			player1: {connect: dto.user_id},
-			player2: {connect: dto.target_id},
-		}
-	});
-
-	return game;
-  }
-
-  async checkGame(id: number) {
-	if (await this.prisma.game.count({where: {id: id}}) == 0)
-	{
-		throw new HttpException({
-			status: HttpStatus.BAD_REQUEST,
-			error: `Game ${id} doesn't exist`,
-		}, HttpStatus.BAD_REQUEST);
+	async getAll() {
+		return await this.prisma.game.findMany();
 	}
-  }
+
+	async get(id: number) {
+		return await this.prisma.game.findUnique({where: {id: id}});
+	}
+
+	async create(dto) {
+		await this.checkUserInGame(dto.user_id);
+		await this.checkUserInGame(dto.target_id);
+
+		const game = await this.prisma.game.create({
+			data: {
+				player1: {connect: {id: dto.user_id}},
+				player2: {connect: {id: dto.target_id}},
+			},
+			include: {
+				player1: true,
+				player2: true,
+			}
+		});
+
+		return game;
+	}
+
+	async checkGame(id: number) {
+		if (await this.prisma.game.count({where: {id: id}}) == 0)
+		{
+			throw new HttpException({
+				status: HttpStatus.BAD_REQUEST,
+				error: `Game ${id} doesn't exist`,
+			}, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	async checkUserInGame(userId: number) {
+		const user = await this.prisma.user.findUnique({
+			where: {id: userId},
+			include: {
+				player1_game: true,
+				player2_game: true,
+			}
+		});
+
+		if (user.player1_game != null || user.player2_game != null)
+		{
+			throw new HttpException({
+				status: HttpStatus.BAD_REQUEST,
+				error: `User ${userId} is already in game`,
+			}, HttpStatus.BAD_REQUEST)
+		}
+	}
 }
 
 
@@ -59,5 +79,4 @@ export class GameService {
 					Return nop
 				OK
 					Cree partie :
-
 */
---
+++ pong_back/src/game/types/game.types.ts
@@ -0,0 +1,11 @@
+export class GameState {
+}
+
+export class GameRoom {
+	id: number;
+
+	player1_id: number;
+	player2_id: number;
+
+	state: GameState;
+}
