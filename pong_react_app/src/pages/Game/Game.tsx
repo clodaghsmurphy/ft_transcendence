376b079b6d31726113163c13ca250ebc986115d3
Adam BEN MCHICHI <adben-mc@e1r6p3.clusters.42paris.fr>
2023-04-07 21:07:16 +0200

    game

--- pong_react_app/src/pages/Game/Game.tsx
+++ pong_react_app/src/pages/Game/Game.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, useContext} from 'react'
 import { Link } from 'react-router-dom'
 import User, { id_to_user } from '../utils/User'
 import NavBar from '../Components/NavBar'
 import { io, Socket } from 'socket.io-client';
 import './Game.css'
+import { AuthContext } from '../../App'
+import axios, { AxiosResponse, AxiosError } from 'axios'
 
 const BALL_SIZE = 10;
 const BALL_SPEED = 10;
@@ -12,66 +14,88 @@ const PADDLE_SPEED = 20;
 
 const { v4: uuidv4 } = require('uuid');
 
-// const socket: Socket = io(`http://${window.location.hostname}:8080/game`);
+type GamePost = {
+	id_user: number,
+}
 
-// socket.on("connect", () => {
-//   console.log("CONNECTED to Game", socket.connected);
-// });
-
-// console.log(socket);
-
-// socket.on("connect_error", (err:any) => {
-// 	console.log(`connect_error due to ${err.message}`);
-// });
+export let socket_game: Socket
 
 function Game() {
 	
-	// useEffect(() => {
-	// 	const handleKeyDown = (event: KeyboardEvent) => {
-	// 		if (event.key === "ArrowUp") {
-	// 			socket.emit('toucheAppuyee', { touche: 'ArrowUp' });
-	// 			console.log("emit ArrowUp");
-	// 		} else if (event.key === "ArrowDown") {
-	// 			socket.emit('toucheAppuyee', { touche: 'ArrowDown' });
-	// 			console.log("emit ArrowDown");
-	// 		} else if (event.key === "w") {	
-	// 			socket.emit('toucheAppuyee', { touche: 'w' });
-	// 			console.log("emit w");
-	// 		} else if (event.key === "s") {
-	// 			socket.emit('toucheAppuyee', { touche: 's' });
-	// 			console.log("emit s");
-	// 		}
-	// 	};
-	// 	document.addEventListener("keydown", handleKeyDown);
-	// 	return () => {
-	// 		document.removeEventListener("keydown", handleKeyDown);
-	// 	};
-	// }, []);
+	const [isJoined, setIsJoined] = useState(false);
+	const { state, dispatch } = useContext(AuthContext);
 
 	const connect = () => {
-		const socket = io(`http://${window.location.hostname}:8080/game`);
-		socket.on("connect", () => {
-			console.log("Connected to game");
-			socket.emit("custom_event", {name: "Bob", age:12})
-			console.log(socket);
+		socket_game = io(`http://${window.location.hostname}:8080/game`,
+		{
+			extraHeaders: {
+				Authorization: "Bearer " + localStorage.getItem('token')
+			}
 		});
+		socket_game.on("connect", () => {
+			console.log("Connected to game");
+			// socket_game.emit("custom_event", {name: "Bob", age:12})
+			console.log(socket_game);
+		});
+		let body: GamePost = {
+			id_user: Number(state.user.id),
+		}
+		axios.post('/api/game/create', body)
+			.then((response: AxiosResponse) => {
+				socket_game.emit('create', {
+					id_user: Number(state.user.id),
+				})
+			})
 	}
-
-	useEffect(() => {
-		connect();
-	}, []);
-
 	
-	  
-
-	return (
-		<div id="game">
-			<div id="leftPaddle"></div>
-			<div id="ball"></div>
-			<div id="score">0-0</div>
-			<div id="rightPaddle"></div>
-		</div>
-	);
+	useEffect(() => {
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.key === "ArrowUp") {
+				socket_game.emit('toucheAppuyee', { touche: 'ArrowUp' });
+				console.log("emit ArrowUp");
+			} else if (event.key === "ArrowDown") {
+				socket_game.emit('toucheAppuyee', { touche: 'ArrowDown' });
+				console.log("emit ArrowDown");
+			} else if (event.key === "w") {	
+				socket_game.emit('toucheAppuyee', { touche: 'w' });
+				console.log("emit w");
+			} else if (event.key === "s") {
+				socket_game.emit('toucheAppuyee', { touche: 's' });
+				console.log("emit s");
+			}
+		};
+		document.addEventListener("keydown", handleKeyDown);
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown);
+		};
+	}, []);
+	
+	const handleJoinGame = async () => {
+		// Appel à la fonction pour rejoindre la partie
+		try {
+			await connect();
+			setIsJoined(true);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	
+	if (!isJoined) {
+		return (
+			<div>
+				<button onClick={handleJoinGame}>Rejoindre la partie</button>
+			</div>
+		);
+	} else {
+		return (
+			<div id="game">
+				<div id="leftPaddle"></div>
+				<div id="ball"></div>
+				<div id="score">0-0</div>
+				<div id="rightPaddle"></div>
+			</div>
+		);
+	}
 }
 
 export default Game;
