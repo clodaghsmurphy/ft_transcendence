7e014a1e6bd9d85e78ec9207868ea2cad7ccb823
Adam BEN MCHICHI <adben-mc@e1r6p6.clusters.42paris.fr>
2023-04-12 02:04:23 +0200

    Debut du responsive, mais besoin de scale le jeu pour ne pas avoir d'effet de distorsion

--- pong_react_app/src/pages/Game/sketches/sketch.js
+++ pong_react_app/src/pages/Game/sketches/sketch.js
@@ -12,22 +12,31 @@ export default function sketch(p5) {
     let score1 = 0,
         score2 = 0;
 
+    const header = document.querySelector('.nav-bar');
+    let header_hauteur = header.offsetHeight;
+    const gameDiv = document.getElementById("game");
+    let div_largeur = gameDiv.offsetWidth,
+    div_hauteur = gameDiv.offsetHeight;
+    if (header_hauteur == div_hauteur)
+        header_hauteur = 0;
+    let game_height = div_hauteur - header_hauteur,
+        game_width = div_largeur;
+
+    let ratio_hauteur = (1 / 400) * game_height,
+        ratio_largeur = (1 / 400) * game_width;
+
     function centerCanvas() {
-        const x = (p5.windowWidth - p5.width) / 2;
-        const y = (p5.windowHeight - p5.height) / 2;
+        const gameDiv = document.getElementById("game");
+        const x = 0;
+        const y = 0;
         canvas.position(x, y);
     }
 
     p5.setup = () => {
-        canvas = p5.createCanvas(400, 400);
+        const gameDiv = document.getElementById("game");
+        canvas = p5.createCanvas(game_width, game_height);
         canvas.parent('game');
-        centerCanvas();
     };
-
-    p5.windowResized = () => {
-        centerCanvas();
-    };
-
 
     p5.draw = () => {
         p5.background(0);
@@ -37,15 +46,48 @@ export default function sketch(p5) {
         p5.fill(p5.color(255, 255, 255));
 
         p5.rectMode(p5.CENTER);
-        p5.rect(20, paddle1, paddleWidth, paddleHeight);
-        p5.rect(p5.width - 20, paddle2, paddleWidth, paddleHeight);
-        p5.rect(ballX, ballY, 20, 20);
+        p5.rect(20 * ratio_largeur, paddle1 * ratio_hauteur, paddleWidth * ratio_largeur, paddleHeight * ratio_hauteur);
+        p5.rect(p5.width - 20 * ratio_largeur, paddle2 * ratio_hauteur, paddleWidth * ratio_largeur, paddleHeight * ratio_hauteur);
+
+        p5.ellipse(ballX * ratio_largeur, ballY * ratio_hauteur, 20 * ratio_largeur , 20 * ratio_hauteur);
+
+
+        p5.ellipse(game_width, game_height, 20, 20);
+
     };
 
     p5.updateWithProps = (props) => {
         if (!props || !props.data) {
             return;
         }
+        
+
+        // Check if resize needed
+        if (div_largeur != gameDiv.offsetWidth || div_hauteur != gameDiv.offsetHeight
+            || header_hauteur != header.offsetHeight)
+        {
+            div_largeur = gameDiv.offsetWidth;
+            div_hauteur = gameDiv.offsetHeight;
+            header_hauteur = header.offsetHeight;
+            if (header_hauteur == div_hauteur)
+                header_hauteur = 0;
+            game_height = div_hauteur - header_hauteur;
+            game_width = div_largeur;
+            console.log("Resive");
+            p5.resizeCanvas(game_width, game_height);
+            ratio_hauteur = (1 / 400) * game_height;
+            ratio_largeur = (1 / 400) * game_width;
+        }
+
+        // let bob = {
+        //     header_hight: header_hauteur,
+        //     div_largeur: div_largeur,
+        //     div_hauteur: div_hauteur,
+        //     header_hauteur: header_hauteur,
+        //     game_height: game_height,
+        //     game_width: game_width,
+        // }
+        // console.log(bob);
 
         let data = props.data;
 
@@ -58,4 +100,5 @@ export default function sketch(p5) {
         ballX = data.ball_pos_x;
         ballY = data.ball_pos_y;
     };
+
 }
