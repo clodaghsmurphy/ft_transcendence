88767af1178e90441d353907d368b1c04f068d9a
Pierre LE LEZ <ple-lez@e1r7p1.clusters.42paris.fr>
2023-04-16 15:19:07 +0200

    Invite via DM

--- pong_back/src/game/game.module.ts
+++ pong_back/src/game/game.module.ts
@@ -3,11 +3,14 @@ import { UserModule } from "src/user/user.module";
 import { GameController } from "./game.controller";
 import { GameGateway } from "./game.gateway";
 import { GameService } from "./game.service";
+import { DmModule } from "src/dm/dm.module";
+import { DmGateway } from "src/dm/dm.gateway";
+import { DmService } from "src/dm/dm.service";
 
 
 @Module({
   controllers: [GameController],
-  providers: [GameService, GameGateway],
-  imports: [UserModule]
+  providers: [GameService, GameGateway, DmService, DmGateway],
+  imports: [UserModule, DmModule]
 })
 export class GameModule {}
