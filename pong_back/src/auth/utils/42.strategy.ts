780662d7cbf0d66095867c3c50a18f571103132c
ple-lez <109950338+ple-lez@users.noreply.github.com>
2023-04-16 21:59:01 +0200

    delete console log

--- pong_back/src/auth/utils/42.strategy.ts
+++ pong_back/src/auth/utils/42.strategy.ts
@@ -19,7 +19,6 @@ export class Ft_Strategy extends PassportStrategy(Strategy) {
 
     async validate(accessToken: string, refreshToken: string, profile: Profile, cb:any)
     {
-        console.log('in 42 strat');
         const userData = {
             name: profile.username,
             id: parseInt(profile.id),
